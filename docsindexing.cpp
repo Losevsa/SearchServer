1df4af4ecba3bcd5c7edf79f5384bb809c7133f2
Losevsa <losevsa@inbox.ru>
2023-04-02 09:42:07 +0300

    1

--- docsindexing.cpp
+++ docsindexing.cpp
@@ -1,7 +1,78 @@
 #include "docsindexing.h"
 
-void addNewFreq(int docId, QMap<QString, QVector<Entry>> *freqDictionary, QMutex* mutex, QString doc)
+void addNewFreq(size_t docId, QMap<QString, QVector<Entry>> *freqDictionary, QMutex* mutex, QString doc)
 {
+    //"выписываем" все слова из doc
+    QRegularExpression rx("\\s+");
+    QStringList words = doc.split(rx, Qt::SkipEmptyParts);
+
+    //лочим для работы с freqDictionary
+    QMutexLocker locker(mutex);
+
+    //каждое слово проверяем c freqDictionary
+    foreach(QString word, words)
+    {
+        bool haveKeys = false;
+        //проходимся по значениям слов в словаре и сравниваем есть ли такие слова
+        QMap<QString, QVector<Entry>>::const_iterator iter = freqDictionary->constBegin();
+        while (iter != freqDictionary->constEnd())
+        {
+            //Если такие слова есть
+            if (iter.key() == word)
+            {
+                haveKeys = true;
+                //Создаем пустой вектор Entry и в него запихиваем, текущий вектор Entry для этого слова
+                QVector<Entry> temp;
+                temp = iter.value();
+
+                bool haveDocId = false;
+                for(int i = 0; i < temp.size(); ++i)
+                {
+                    //ищем такой же документ, если есть, то плюсуем
+                    if (temp[i].docId == docId)
+                    {
+                        temp[i].count++;
+                        haveDocId = true;
+                    }
+                }
+
+                if (!haveDocId)
+                {
+                    Entry tempEntry;
+                    tempEntry.docId = docId;
+                    tempEntry.count = 1;
+                    temp.push_back(tempEntry);
+                }
+
+
+
+
+            }
+            qDebug() << iter.key() << ": " << &iter.value();
+            iter++;
+        }
+
+        if (!haveKeys)
+        {
+            Entry tempEntry;
+            tempEntry.docId = docId;
+            tempEntry.count = 1;
+
+        }
+        /*
+        for(auto it = freqDictionary->begin(); it != freqDictionary->end(); ++it)
+        {
+
+            QVector<Entry> entries = it.value();
+            for(int i = 0; i < entries.size(); ++i) {
+                Entry entry = entries.at(i);
+                //qDebug() << "Doc ID:" << entry.doc_id << "Count:" << entry.count;
+            }
+        }
+        */
+    }
+
+    /*
     Entry test;
     test.count = 2;
     test.docId = 3;
@@ -12,7 +83,7 @@ void addNewFreq(int docId, QMap<QString, QVector<Entry>> *freqDictionary, QMutex
 
     QMutexLocker locker(mutex);
     freqDictionary->insert("asdf", testTwo);
-
+    */
 }
 
 DocsIndexing::DocsIndexing()
@@ -52,6 +123,7 @@ void DocsIndexing::addFreqThreaded()
             future.waitForFinished();
         }
 
+        /*
         if (threadsCount == 5)
         {
             for (auto& future : threads) {
@@ -61,6 +133,7 @@ void DocsIndexing::addFreqThreaded()
             threadsCount = 0;
             threads.clear();
         }
+        */
     }
 
 
